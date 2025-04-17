79a3b5560acbf03cfea03541afc242a10339318a
Abdallah Shalat <85385315+abdmosh2000@users.noreply.github.com>
2025-04-17 11:52:32 +0300

    add api

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const app = require('./app');
 const PORT = process.env.PORT || 5000;
 
 
+app.get("/", (req, res) => {
+  res.send("✅ Memorix API is online!");
+});
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
